6667aade6026cb3acb022e3a51e70d04da98924f
msa46 <mohamad.sanaye462@gmail.com>
2025-05-23 07:36:20 +0200

    Add a boilerplate for mocking the alert

--- src/main/java/com/alerts/Alert.java
+++ src/main/java/com/alerts/Alert.java
@@ -23,4 +23,34 @@ public class Alert {
     public long getTimestamp() {
         return timestamp;
     }
+    
+    public String getMessage() {
+        // Format the message based on the condition
+        if (condition.equals("High Diastolic")) {
+            return "High Blood Pressure Diastolic";
+        } else if (condition.equals("Low Diastolic")) {
+            return "Low Blood Pressure Diastolic";
+        } else if (condition.equals("High Systolic")) {
+            return "High Blood Pressure Systolic";
+        } else if (condition.equals("Low Systolic")) {
+            return "Low Blood Pressure Systolic";
+        } else if (condition.equals("Critical Hypertension")) {
+            return "Critical Blood Pressure - Critical Hypertension";
+        } else if (condition.equals("Fluctuating")) {
+            return "Blood Pressure Alert - Fluctuating";
+        } else if (condition.equals("Critical")) {
+            return "Critical Blood Saturation";
+        } else if (condition.equals("High Difference")) {
+            return "High Blood Saturation Difference";
+        } else if (condition.equals("Decreasing Trend")) {
+            return "Blood Oxygen Alert - Decreasing Trend";
+        } else if (condition.equals("Irregular Rhythm")) {
+            return "ECG Alert - Irregular Rhythm";
+        } else if (condition.equals("Tachycardia")) {
+            return "ECG Alert - Tachycardia";
+        } else {
+            // Default case for other types of alerts
+            return condition;
+        }
+    }
 }
